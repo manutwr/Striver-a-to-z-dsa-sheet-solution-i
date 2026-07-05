eafba228730c65e352dbd3ca072ecd07cde8e337
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-05 12:08:09 +0530

    Create 4.pp

---
+++ Pattern printing/All question/4.pp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void pattern4(int n) {
+       for (int i= 1 ; i<= n; i++){
+        for (int  j= 1; j<=i ; j++
+        ){
+            cout << i;
+        }
+        cout << endl;
+       }
+    }
+};
+
+
+
+
+//
+1
+22
+333
+4444
+55555
