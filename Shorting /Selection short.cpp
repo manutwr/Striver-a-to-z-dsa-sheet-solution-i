c97f4e2ff44e5c0bd52f0de03f54a576c7ab9296
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-18 11:54:17 +0530

    Update Selection short.cpp

--- Shorting /Selection short.cpp
+++ Shorting /Selection short.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> selectionSort(vector<int>& arr) {
-       int n = arr.size();
-        for (int i= 0 ;  i< n-1 ; i++){
-             int smallest = i;
-            for (int j =i+1 ; j<n ; j++ ){
-                if (arr[j]< arr [smallest]){
-                    smallest = j;
+       int n = arr.size();  // array ka size declear
+        for (int i= 0 ;  i< n-1 ; i++){ // loop chalaya last tak 
+             int smallest = i; // assume kiya ki i smallest hai 
+            for (int j =i+1 ; j<n ; j++ ){ // fir ek loop chalaya smallest dhundhne ke lie 
+                if (arr[j]< arr [smallest]){ // agar hamara smallest element jo ki i tha woh bada ho gya j se mtlb koi aur element hai jo smallest s bhi small h;
+                    smallest = j; // fir hmne smallest ko update krke  daal dia smallest me;
                 }
                 
             }
-             swap (arr[i],arr[smallest])  ;
+             swap (arr[i],arr[smallest])  ; // ek swap kia smallest ko array i se kyuki array short ho.... array ke index me sbse smallest value put ki kykui index zero me sbse smallest value hi aati hai;;;;; aur fir loop run krwaya again and again 
         }
         return arr;
     
