581fedb29b9d79433436a0c90950d30f48aab941
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-25 10:13:23 +0530

    Update Second largest number.cpp

--- ARRAYS/EASY/Second largest number.cpp
+++ ARRAYS/EASY/Second largest number.cpp
@@ -13,11 +13,11 @@ public:
       // for second largest 
       int ans2 = INT_MIN ;
       for (int i = 0 ; i<n ; i++ ){
-        if (arr[i]!= ans1){
-            ans2 = max(ans2 , arr[i]);
+        if (arr[i]!= ans1){ // second largest number first largest ke barabar nahi hona chahiye 
+            ans2 = max(ans2 , arr[i]); // to max me ans2 daal do aur arr[i] me daal do
         }
       }
-      if (ans2 == INT_MIN){
+      if (ans2 == INT_MIN){ // agar hame scond largest nahi milta hai to hm 1 eturn kr dnege
         return -1;
       }
       return ans2;
