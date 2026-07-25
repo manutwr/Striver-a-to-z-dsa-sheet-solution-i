1c96e5c00d519b0cef23a239186cd4089d7cf78a
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-25 09:52:34 +0530

    Update Largest element.cpp

--- ARRAYS/EASY/Largest element.cpp
+++ ARRAYS/EASY/Largest element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int largestElement(vector<int>& arr ) {
-        int n = arr.size();
-     int ans = INT_MIN;
-     for (int i = 0; i<n; i++){
-        if (arr[i]>ans  ){
-            ans = arr[i];
+        int n = arr.size(); //array ka size de clear kro
+     int ans = INT_MIN; // ek minimum value o assume karo 
+     for (int i = 0; i<n; i++){ // ek loop chalao
+        if (arr[i]>ans  ){ // agar array ka ki element answer se bada payaa gyaa to usee ans me put kr do
+            ans = arr[i]; // put the element in aswer 
         }
      }
-     return ans;
+     return ans; // as a answer return kar do 
     }
 };
