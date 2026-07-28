ca611073a3299e360e03167aee13ccdf1ed75106
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-28 09:25:46 +0530

    Update Check if array is shorted.cpp

--- ARRAYS/EASY/Check if array is shorted.cpp
+++ ARRAYS/EASY/Check if array is shorted.cpp
@@ -1,1 +1,14 @@
-
+class Solution{	
+	public:
+		bool isSorted(vector<int>& arr ){
+			//your code goes here
+          for (int i = 0 ; i < arr.size ()-1  ; i++ ){
+            if ( arr [i]> arr[i+1]){
+            return false ;
+          }
+    
+		} 
+      
+         return true;
+}
+};
